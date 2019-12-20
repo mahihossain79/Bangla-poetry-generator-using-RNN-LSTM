4f15a460f541eefbea5654c002f237633b128193
Shakhawat Hossain Mahi <43726963+mahihossain79@users.noreply.github.com>
2019-12-20 20:24:09 +0600

    final code
    This code generates a line from the input line of generated.txt .

---
+++ Shakhawat Hossain Mahi/poem.py
@@ -0,0 +1,87 @@
+
+from __future__ import print_function
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.utils.data_utils import get_file
+import numpy as np
+import random
+import sys
+
+text = open("nazrul.txt",'r',encoding = 'utf-8').read() 
+print('length:', len(text))
+# Creating a mapping from unique characters to indices
+chars = set(text)
+print('total characters:', len(chars))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+# cut the text in semi-redundant sequences of maxlen characters
+maxlen = 40
+step = 3
+sentences = []
+next_chars = []
+for i in range(0, len(text) - maxlen, step):
+    sentences.append(text[i: i + maxlen])
+    next_chars.append(text[i + maxlen])
+print('Sequences:', len(sentences))
+
+
+X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+
+# build the model: 2 stacked LSTM
+#print('Building model...')
+model = Sequential()
+model.add(LSTM(512, return_sequences=True, input_shape=(maxlen, len(chars))))
+model.add(Dropout(0.2))
+model.add(LSTM(512, return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+
+def sample(a, temperature=1.0):
+    # helper function to sample an index from a probability array
+    a = np.log(a) / temperature
+    a = np.exp(a) / np.sum(np.exp(a))
+    return np.argmax(np.random.multinomial(1, a, 1))
+
+model.fit(X, y, batch_size=64, nb_epoch=1)
+model.load_weights('weights-33.h5')
+start_index = 0
+model.summary()
+
+
+seed_text =open("generated.txt",'r',encoding = 'utf-8').read() 
+
+
+for j in [0.5, 1.0]:
+    generated = ''
+    sentence = seed_text[start_index: start_index + maxlen]
+    generated += sentence
+    print('Generated Lines "' + sentence + '"')
+    sys.stdout.write(generated)
+
+    for i in range(400):
+        x = np.zeros((1, maxlen, len(chars)))
+        for t, char in enumerate(sentence):
+            x[0, t, char_indices[char]] = 1.
+
+        preds = model.predict(x, verbose=0)[0]
+        next_index = sample(preds, j)
+        next_char = indices_char[next_index]
+
+        generated += next_char
+        sentence = sentence[1:] + next_char
+
+        sys.stdout.write(next_char)
+        sys.stdout.flush()
+    print()
